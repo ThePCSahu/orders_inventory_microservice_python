b0578a7b04f40624af3c6f62513fba0ded1e8503
Poonamchand Sahu <>
2025-11-15 21:19:41 +0530

    Fix DB issue

--- src/app/database/__init__.py
+++ src/app/database/__init__.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import sessionmaker
 
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./test.db")
+DATABASE_URL = os.getenv("DATABASE_URL", "sqlite:///./database.db")
 
 engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
